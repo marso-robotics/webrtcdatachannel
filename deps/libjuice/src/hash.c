2f7f49744b32d744382402e09e64e2a4cba7341a
jasjeremy <jeremy.jaspar@marsorobotics.com>
2026-02-23 11:06:13 +0100

    fix(webrtc): using openssl encryption + logs

--- deps/libjuice/src/hash.c
+++ deps/libjuice/src/hash.c
@@ -12,24 +12,15 @@
 #include <nettle/md5.h>
 #include <nettle/sha1.h>
 #include <nettle/sha2.h>
-#elif defined(_WIN32)
-#include <windows.h>
-#include <bcrypt.h>
 #else
-#include "picohash.h"
+#include <openssl/evp.h>
 #endif
 
-#if defined(_WIN32) && !USE_NETTLE
-static void bcrypt_hash(const wchar_t *algorithm, ULONG hash_size,
-                        const void *message, size_t size, void *digest) {
-	BCRYPT_ALG_HANDLE hAlg = NULL;
-	BCRYPT_HASH_HANDLE hHash = NULL;
-	BCryptOpenAlgorithmProvider(&hAlg, algorithm, NULL, 0);
-	BCryptCreateHash(hAlg, &hHash, NULL, 0, NULL, 0, 0);
-	BCryptHashData(hHash, (PUCHAR)message, (ULONG)size, 0);
-	BCryptFinishHash(hHash, (PUCHAR)digest, hash_size, 0);
-	if (hHash) BCryptDestroyHash(hHash);
-	if (hAlg)  BCryptCloseAlgorithmProvider(hAlg, 0);
+#if !USE_NETTLE
+static void openssl_hash(const EVP_MD *md, unsigned int hash_size,
+                         const void *message, size_t size, void *digest) {
+	unsigned int len = hash_size;
+	EVP_Digest(message, size, (unsigned char *)digest, &len, md, NULL);
 }
 #endif
 
@@ -39,13 +30,8 @@ void hash_md5(const void *message, size_t size, void *digest) {
 	md5_init(&ctx);
 	md5_update(&ctx, size, message);
 	md5_digest(&ctx, HASH_MD5_SIZE, digest);
-#elif defined(_WIN32)
-	bcrypt_hash(BCRYPT_MD5_ALGORITHM, 16, message, size, digest);
 #else
-	picohash_ctx_t ctx;
-	picohash_init_md5(&ctx);
-	picohash_update(&ctx, message, size);
-	picohash_final(&ctx, digest);
+	openssl_hash(EVP_md5(), 16, message, size, digest);
 #endif
 }
 
@@ -55,13 +41,8 @@ void hash_sha1(const void *message, size_t size, void *digest) {
 	sha1_init(&ctx);
 	sha1_update(&ctx, size, message);
 	sha1_digest(&ctx, HASH_SHA1_SIZE, digest);
-#elif defined(_WIN32)
-	bcrypt_hash(BCRYPT_SHA1_ALGORITHM, 20, message, size, digest);
 #else
-	picohash_ctx_t ctx;
-	picohash_init_sha1(&ctx);
-	picohash_update(&ctx, message, size);
-	picohash_final(&ctx, digest);
+	openssl_hash(EVP_sha1(), 20, message, size, digest);
 #endif
 }
 
@@ -71,12 +52,7 @@ void hash_sha256(const void *message, size_t size, void *digest) {
 	sha256_init(&ctx);
 	sha256_update(&ctx, size, message);
 	sha256_digest(&ctx, HASH_SHA256_SIZE, digest);
-#elif defined(_WIN32)
-	bcrypt_hash(BCRYPT_SHA256_ALGORITHM, 32, message, size, digest);
 #else
-	picohash_ctx_t ctx;
-	picohash_init_sha256(&ctx);
-	picohash_update(&ctx, message, size);
-	picohash_final(&ctx, digest);
+	openssl_hash(EVP_sha256(), 32, message, size, digest);
 #endif
 }
