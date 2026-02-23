9fafc1cbcf2eb3ecf5b3ca8a89ce0f9616fc0b01
jasjeremy <jeremy.jaspar@marsorobotics.com>
2026-02-23 11:07:25 +0100

    fix(webrtc): using openssl encryption + logs

--- deps/libjuice/src/hash.c
+++ deps/libjuice/src/hash.c
@@ -12,28 +12,11 @@
 #include <nettle/md5.h>
 #include <nettle/sha1.h>
 #include <nettle/sha2.h>
-#elif defined(_WIN32)
-#include <windows.h>
-#include <bcrypt.h>
 #else
 #include <openssl/evp.h>
 #endif
 
-#if defined(_WIN32) && !USE_NETTLE
-static void bcrypt_hash(const wchar_t *algorithm, ULONG hash_size,
-                        const void *message, size_t size, void *digest) {
-	BCRYPT_ALG_HANDLE hAlg = NULL;
-	BCRYPT_HASH_HANDLE hHash = NULL;
-	BCryptOpenAlgorithmProvider(&hAlg, algorithm, NULL, 0);
-	BCryptCreateHash(hAlg, &hHash, NULL, 0, NULL, 0, 0);
-	BCryptHashData(hHash, (PUCHAR)message, (ULONG)size, 0);
-	BCryptFinishHash(hHash, (PUCHAR)digest, hash_size, 0);
-	if (hHash) BCryptDestroyHash(hHash);
-	if (hAlg)  BCryptCloseAlgorithmProvider(hAlg, 0);
-}
-#endif
-
-#if !defined(_WIN32) && !USE_NETTLE
+#if !USE_NETTLE
 static void openssl_hash(const EVP_MD *md, unsigned int hash_size,
                          const void *message, size_t size, void *digest) {
 	unsigned int len = hash_size;
@@ -47,8 +30,6 @@ void hash_md5(const void *message, size_t size, void *digest) {
 	md5_init(&ctx);
 	md5_update(&ctx, size, message);
 	md5_digest(&ctx, HASH_MD5_SIZE, digest);
-#elif defined(_WIN32)
-	bcrypt_hash(BCRYPT_MD5_ALGORITHM, 16, message, size, digest);
 #else
 	openssl_hash(EVP_md5(), 16, message, size, digest);
 #endif
@@ -60,8 +41,6 @@ void hash_sha1(const void *message, size_t size, void *digest) {
 	sha1_init(&ctx);
 	sha1_update(&ctx, size, message);
 	sha1_digest(&ctx, HASH_SHA1_SIZE, digest);
-#elif defined(_WIN32)
-	bcrypt_hash(BCRYPT_SHA1_ALGORITHM, 20, message, size, digest);
 #else
 	openssl_hash(EVP_sha1(), 20, message, size, digest);
 #endif
@@ -73,8 +52,6 @@ void hash_sha256(const void *message, size_t size, void *digest) {
 	sha256_init(&ctx);
 	sha256_update(&ctx, size, message);
 	sha256_digest(&ctx, HASH_SHA256_SIZE, digest);
-#elif defined(_WIN32)
-	bcrypt_hash(BCRYPT_SHA256_ALGORITHM, 32, message, size, digest);
 #else
 	openssl_hash(EVP_sha256(), 32, message, size, digest);
 #endif
--- deps/libjuice/src/hmac.c
+++ deps/libjuice/src/hmac.c
@@ -10,9 +10,6 @@
 
 #if USE_NETTLE
 #include <nettle/hmac.h>
-#elif defined(_WIN32)
-#include <windows.h>
-#include <bcrypt.h>
 #else
 #include <openssl/hmac.h>
 #include <openssl/evp.h>
@@ -25,37 +22,20 @@ static int hmac_impl_logged = 0;
 void hmac_sha1(const void *message, size_t size, const void *key, size_t key_size, void *digest) {
 	if (!hmac_impl_logged) {
 		hmac_impl_logged = 1;
-		/* RFC 2202 test case 2: key="Jefe", data="what do ya want for nothing?" */
 		uint8_t test_out[20];
 		const char *test_key = "Jefe";
 		const char *test_data = "what do ya want for nothing?";
-		/* Expected: effcdf6ae5eb2fa2d27416d5f184df9c259a7c79 */
 		static const uint8_t expected[20] = {
 			0xef,0xfc,0xdf,0x6a,0xe5,0xeb,0x2f,0xa2,0xd2,0x74,
 			0x16,0xd5,0xf1,0x84,0xdf,0x9c,0x25,0x9a,0x7c,0x79
 		};
 #if USE_NETTLE
 		fprintf(stderr, "[HMAC-IMPL] Using Nettle");
-#elif defined(_WIN32)
-		fprintf(stderr, "[HMAC-IMPL] Using BCrypt");
-#else
-		fprintf(stderr, "[HMAC-IMPL] Using OpenSSL");
-#endif
-		/* Compute the test vector using the actual implementation below,
-		   but we need to call ourself recursively - avoid that, just
-		   inline the implementation here. */
-#if USE_NETTLE
 		{ struct hmac_sha1_ctx c; hmac_sha1_set_key(&c,4,(const uint8_t*)test_key);
 		  hmac_sha1_update(&c,28,(const uint8_t*)test_data);
 		  hmac_sha1_digest(&c,20,test_out); }
-#elif defined(_WIN32)
-		{ BCRYPT_ALG_HANDLE a=NULL; BCRYPT_HASH_HANDLE h=NULL;
-		  BCryptOpenAlgorithmProvider(&a,BCRYPT_SHA1_ALGORITHM,NULL,BCRYPT_ALG_HANDLE_HMAC_FLAG);
-		  BCryptCreateHash(a,&h,NULL,0,(PUCHAR)test_key,4,0);
-		  BCryptHashData(h,(PUCHAR)test_data,28,0);
-		  BCryptFinishHash(h,test_out,20,0);
-		  if(h)BCryptDestroyHash(h); if(a)BCryptCloseAlgorithmProvider(a,0); }
 #else
+		fprintf(stderr, "[HMAC-IMPL] Using OpenSSL");
 		{ unsigned int ml=20;
 		  HMAC(EVP_sha1(),test_key,4,(const unsigned char*)test_data,28,test_out,&ml); }
 #endif
@@ -69,15 +49,6 @@ void hmac_sha1(const void *message, size_t size, const void *key, size_t key_siz
 	hmac_sha1_set_key(&ctx, key_size, key);
 	hmac_sha1_update(&ctx, size, message);
 	hmac_sha1_digest(&ctx, HMAC_SHA1_SIZE, digest);
-#elif defined(_WIN32)
-	BCRYPT_ALG_HANDLE hAlg = NULL;
-	BCRYPT_HASH_HANDLE hHash = NULL;
-	BCryptOpenAlgorithmProvider(&hAlg, BCRYPT_SHA1_ALGORITHM, NULL, BCRYPT_ALG_HANDLE_HMAC_FLAG);
-	BCryptCreateHash(hAlg, &hHash, NULL, 0, (PUCHAR)key, (ULONG)key_size, 0);
-	BCryptHashData(hHash, (PUCHAR)message, (ULONG)size, 0);
-	BCryptFinishHash(hHash, (PUCHAR)digest, 20, 0);
-	if (hHash) BCryptDestroyHash(hHash);
-	if (hAlg)  BCryptCloseAlgorithmProvider(hAlg, 0);
 #else
 	unsigned int md_len = HMAC_SHA1_SIZE;
 	HMAC(EVP_sha1(), key, (int)key_size,
@@ -92,15 +63,6 @@ void hmac_sha256(const void *message, size_t size, const void *key, size_t key_s
 	hmac_sha256_set_key(&ctx, key_size, key);
 	hmac_sha256_update(&ctx, size, message);
 	hmac_sha256_digest(&ctx, HMAC_SHA256_SIZE, digest);
-#elif defined(_WIN32)
-	BCRYPT_ALG_HANDLE hAlg = NULL;
-	BCRYPT_HASH_HANDLE hHash = NULL;
-	BCryptOpenAlgorithmProvider(&hAlg, BCRYPT_SHA256_ALGORITHM, NULL, BCRYPT_ALG_HANDLE_HMAC_FLAG);
-	BCryptCreateHash(hAlg, &hHash, NULL, 0, (PUCHAR)key, (ULONG)key_size, 0);
-	BCryptHashData(hHash, (PUCHAR)message, (ULONG)size, 0);
-	BCryptFinishHash(hHash, (PUCHAR)digest, 32, 0);
-	if (hHash) BCryptDestroyHash(hHash);
-	if (hAlg)  BCryptCloseAlgorithmProvider(hAlg, 0);
 #else
 	unsigned int md_len = HMAC_SHA256_SIZE;
 	HMAC(EVP_sha256(), key, (int)key_size,
